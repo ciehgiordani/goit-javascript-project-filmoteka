16b725701314a15c2eae26ce8f542f57c464ed7a
ciehgiordani <acieh_giordani@yahoo.com>
2024-02-23 16:37:40 +0800

    first commit

---
+++ src/js/team.js
@@ -0,0 +1,47 @@
+
+export const teamList = [
+    {
+        name: 'Perci Aceron',
+        role: 'Team Lead - Loader and Additional Functionalities',
+        github: 'https://perci-aceron.github.io/',
+        photo: '../images/team/Perci.JPG',
+    },
+    {
+        name: 'Kate Limosnero',
+        role: 'Scrum Master - libraries',
+        github: 'https://github.com/katelim215',
+        photo:'../images/team/Kate.JPG' ,
+    },
+    {
+        name: 'Mon Laguatan',
+        role: 'Developer - UI/UX design',
+        github: 'https://github.com/kiksmon21',
+        photo: '../images/team/Mon.JPG',
+    },
+    {
+        name: 'Rey Leal',
+        role: 'Developer - main page',
+        github: 'https://github.com/reyleal27',
+        photo: '../images/team/Rey.JPG',
+    },
+    {
+        name: 'Jessah Seroje',
+        role: 'Developer - libraries',
+        github: '',
+        photo: '../images/team/Jessa.JPG',
+    },
+    {
+        name: 'Bri Lang',
+        role: 'Developer - movie cards, modals',
+        github: ' https://github.com/Bmorta',
+        photo: '../images/team/Bri.JPG' ,
+    },
+    {
+        name: 'Franci Giordani',
+        role: 'Developer - movie cards, modals',
+        github: '',
+        photo: '../images/team/franci.jpg',
+    }
+    
+
+]
